b7f125ef1eb9f55306cad95336775a29f8eda4fd
Steve Kitzes <SteveK@1099pro.com>
2019-06-18 12:48:04 -0700

    first attempt at an icon in ExpandableSelector, added icon attrib in footer

--- src/components/ExpandableSelector.js
+++ src/components/ExpandableSelector.js
@@ -4,20 +4,28 @@ import CONSTANTS from '../constants';
 
 function ExpandableSelector(props) {
     return (
-        <div style={expandableSelector}>
-            thing <img src={CONSTANTS.images.caretBlack} />
+        <div style={expandableSelectorStyle}>
+            <img style={iconStyle} src='/img/activity/custom-by-egor-rumyantsev.svg' /> <img style={caretStyle} src={CONSTANTS.images.caretBlack} />
         </div>
     );
 }
 
-const expandableSelector = {
-    backgroundColor: 'lightgray',
-    border: '.1rem solid gray',
+const expandableSelectorStyle = {
+    backgroundColor: 'white',
+    border: '.1rem solid lightgray',
     borderRadius: '.25rem',
     cursor: 'pointer',
     display: 'inline-block',
     margin: '-.2rem 0 0 0',
     padding: '.2rem .4rem'
+}
+const iconStyle = {
+    height: '1rem',
+    margin: '-.3rem 0 0 0',
+    width: '1rem'
+}
+const caretStyle = {
+    margin: '-.3rem 0 0 0'
 }
 
 export default ExpandableSelector;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,12 @@ function Footer({theme}) {
     return (
         <div style={getFooterStyle(themeColors)}>
             <div style={getSeparatorStyle(themeColors)} />
-            <p style={pStyle}>This spiffy travel planning helper brought to you free as Open Source Software under the MIT License!  :)  Yay!</p>
+            <span>
+                This spiffy travel planning helper brought to you free as Open Source Software under the MIT License!  :)  Yay!
+            </span><br />
+            <span>
+                Icons made by Egor Rumyantsev, Gregor Cresnar, Google, and Freepik from <a target="_blank" href='https://www.flaticon.com'>www.flaticon.com</a>
+            </span>
         </div>
     );
 }
@@ -21,8 +26,11 @@ function getFooterStyle(colors) {
         backgroundColor: colors.footerBg,
         bottom: '0',
         color: colors.text,
-        height: '3em',
+        fontSize: '.6rem',
+        // height: '3em',
         left: '0',
+        lineHeight: '.75rem',
+        paddingBottom: '.25rem',
         position: 'fixed',
         width: '100%'
     };
@@ -32,11 +40,8 @@ function getSeparatorStyle(colors) {
         backgroundColor: colors.heavy,
         height: '.3em',
         left: '0',
-        margin: '0'
+        margin: '0 0 .25rem 0'
     };
-}
-const pStyle = {
-    marginTop: '.7em'
 }
 
 export default Footer;
