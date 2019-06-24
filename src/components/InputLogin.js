10c4045e259f85bed6a3ad9b55860d7aa5678294
Steven Kitzes <steven.kitzes@gmail.com>
2019-06-23 17:32:04 -0700

    added input reading and passwd masking for login screen

--- src/components/InputLogin.js
+++ src/components/InputLogin.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import CONSTANTS from '../constants';
 
-function InputLogin({placeholder, value, valueModifier, theme}) {
+function InputLogin({placeholder, value, valueModifier, theme, password}) {
     function handleChange(event) {
         valueModifier(event.target.value);
     }
     return (
         <input
+            type={password ? 'password' : 'text'}
             className={theme === CONSTANTS.dark ?
                 'login-input input-dark' :
                 'login-input input-light'}
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 import CONSTANTS from '../constants';
@@ -11,6 +11,15 @@ function Login({theme}) {
     const themeColors = theme === CONSTANTS.dark ?
         CONSTANTS.colors.dark :
         CONSTANTS.colors.light;
+
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    function handleEmailChange(newEmail) {
+        setEmail(newEmail);
+    }
+    function handlePassChange(newPass) {
+        setPass(newPass);
+    }
 
     return (
         <div style={getLoginStyle(themeColors)}>
@@ -25,18 +34,19 @@ function Login({theme}) {
                 <InputLogin
                     theme={theme}
                     placeholder='Email'
-                    value={''}
-                    valueModifier={null} /><br />
+                    value={email}
+                    valueModifier={handleEmailChange} /><br />
             </div>
             <div style={{margin: '1rem 0 0 0', position: 'relative'}}>
                 <InputHeader id='password-header' theme={theme}>
                     Password
                 </InputHeader>
                 <InputLogin
+                    password
                     theme={theme}
                     placeholder='Password'
-                    value={''}
-                    valueModifier={null} />{' '}
+                    value={pass}
+                    valueModifier={handlePassChange} />{' '}
             </div>
         </div>
     );
